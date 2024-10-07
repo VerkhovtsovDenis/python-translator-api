b38c60660aba2809172f0d2d3b50ef03d99eedce
Omelchenko Maksim <omelchenko.ma@dvfu.ru>
2024-10-07 18:48:00 +1000

    add some examples of lexical analysis

--- main.py
+++ main.py
@@ -1,5 +1,48 @@
-import LexicalAnalyzer
-import SemanticalAnalyzer
+from LexicalAnalyzer import Lexer
+from LexicalAnalyzer.TokenType import TOKEN_TYPES_LIST
+
+example_programms = [
+    '''
+    begin
+        writeln('Привет, мир!');
+    end.
+    ''',
+
+    '''
+    var
+        a,b: real;
+        r: real;
+    begin
+        write('Введите координату точки a: ');
+        readln(a);
+        write('Введите координату точки b: ');
+        readln(b);
+        r := abs(a-b);
+        writeln('Расстояние между точками = ', r);
+    end.
+    ''',
+]
+
+
+def parse_programm(programm):
+    lexer = Lexer(inpt=programm)
+    ignore_tokens = (
+        TOKEN_TYPES_LIST.get('Newline'),
+        TOKEN_TYPES_LIST.get('Whitesapce'),
+        TOKEN_TYPES_LIST.get('Tabulation'),
+    )
+
+    print('our code:')
+    print('--------')
+    print(programm)
+    print('--------\n')
+    print('Tokens:')
+
+    for token in lexer.tokenize():
+
+        if token.token_type not in ignore_tokens:
+            print(token)
+
 
 if __name__ == "__main__":
-    pass
+    [parse_programm(programm) for programm in example_programms]
