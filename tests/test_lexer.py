b0b56adaad3a8995b4f4c7ab6ae40f6aeef270ac
Denis Verkhovtcov <verkhovtcov.do@dvfu.ru>
2024-10-10 17:09:25 +1000

    second test - test func tokineze, real code

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -1,5 +1,8 @@
 # flake8: noqa: F401
-import pytest
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from LexicalAnalyzer import Lexer, FileManager, TokenTypes
 
 def test_can_lexer_init():
@@ -10,5 +13,56 @@ def test_can_lexer_init():
 def test_get_lexem_1pas():
     path = r'\tests\lexer_code\1.pas'
     pascal_code = FileManager.get_code(path)
-    tokens = Lexer(code=pascal_code).tokenize()
-    print(tokens)
+    tokens = list(Lexer(code=pascal_code).tokenize())
+    delimeters_tokens = tokens
+
+    correct_tokens_subsequence = [
+        TokenTypes.WHITESPACE,
+        TokenTypes.BEGIN,
+        TokenTypes.NEWLINE,
+        TokenTypes.WHITESPACE,
+        TokenTypes.WRITELN,
+        TokenTypes.LEFT_BRACKET,
+        TokenTypes.STRING,
+        TokenTypes.RIGHT_BRACKET, 
+        TokenTypes.SEMICOLON,
+        TokenTypes.WHITESPACE,
+        TokenTypes.SINGLE_LINE_COMMENT,
+        TokenTypes.WHITESPACE, 
+        TokenTypes.END,
+        TokenTypes.DOT
+    ]
+
+    for token, corrett_token_type in zip(delimeters_tokens, correct_tokens_subsequence):
+        assert token.token_type == corrett_token_type
+
+def test_get_lexem_2pas():
+    path = r'\tests\lexer_code\1.pas'
+    pascal_code = FileManager.get_code(path)
+    tokens = list(Lexer(code=pascal_code).tokenize())
+    delimeters_tokens = tokens
+
+    correct_tokens_subsequence = [
+        TokenTypes.WHITESPACE,
+        TokenTypes.BEGIN,
+        TokenTypes.NEWLINE,
+        TokenTypes.WHITESPACE,
+        TokenTypes.WRITELN,
+        TokenTypes.LEFT_BRACKET,
+        TokenTypes.STRING,
+        TokenTypes.RIGHT_BRACKET, 
+        TokenTypes.SEMICOLON,
+        TokenTypes.WHITESPACE,
+        TokenTypes.SINGLE_LINE_COMMENT,
+        TokenTypes.WHITESPACE, 
+        TokenTypes.END,
+        TokenTypes.DOT
+    ]
+
+    for token, corrett_token_type in zip(delimeters_tokens, correct_tokens_subsequence):
+        assert token.token_type == corrett_token_type
+
+
+if __name__ == "__main__":
+    test_get_lexem_1pas()
+    
--- tests/test_token.py
+++ tests/test_token.py
@@ -1,5 +1,4 @@
 # flake8: noqa: F401
-# import pytest
 import sys
 import os
 
