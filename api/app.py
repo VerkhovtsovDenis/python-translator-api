b6cb17661a2f9ab8476f5362dc1b0ddbb2b8f874
maksim <you@example.com>
2025-03-30 11:41:17 +1000

    Добавлены манифесты для деплоя и изменен префикс url(добавлено translator)

--- api/app.py
+++ api/app.py
@@ -3,16 +3,20 @@ from Translator import Translator
 from constants import SupportLanguages
 from .schemas import TranslateInput, TranslateOutput
 
+APP_PREFIX ="translator"
+
 app = FastAPI(
     title="PascalTranslate API",
     description=(
         "PascalTranslate API — это FastAPI приложение для трансляции "
         "pascal в другие языки программирования."
     ),
+    docs_url=f"/{APP_PREFIX}/docs",
+    openapi_url=f"/{APP_PREFIX}/openapi.json",
 )
 
 
-@app.post("/translate")
+@app.post(f"/{APP_PREFIX}/translate")
 async def translate(translate_data: TranslateInput) -> TranslateOutput:
     try:
         result_code = Translator.pascla_translate(
--- dockerfile
+++ dockerfile
@@ -8,4 +8,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . /translator_app/
 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "5000", "--reload"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
